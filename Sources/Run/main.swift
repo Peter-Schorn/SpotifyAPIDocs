1963cf3adce828951192646e80a0326fd77dc886
Peter-Schorn <petervschorn@gmail.com>
2021-07-02 19:38:09 -0500

    Reverted to default swift version. Use hardcoded path to bundle.

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -9,9 +9,25 @@ defer { app.shutdown() }
 app.http.server.configuration.hostname = "0.0.0.0"
 
 
+#if DEBUG
+
 guard let docCArchiveURL = docCArchiveURL else {
     fatalError("could not find SpotifyWebAPI.doccarchive in bundle")
 }
+
+#else  // RELEASE configuration on heroku
+
+let path = "/app/.swift-bin/SpotifyAPIDocs_VaporDocC.resources"
+guard let bundle = Bundle(path: path) else {
+    fatalError("could not create bundle from path \(path)")
+}
+guard let docCArchiveURL = bundle.url(
+    forResource: "SpotifyWebAPI", withExtension: "doccarchive"
+) else {
+    fatalError("could not find SpotifyWebAPI.doccarchive in bundle")
+}
+
+#endif
 
 let redirectRoot = "documentation/SpotifyWebAPI"
 let redirectMissingTrailingSlash = ProcessInfo.processInfo.environment[
