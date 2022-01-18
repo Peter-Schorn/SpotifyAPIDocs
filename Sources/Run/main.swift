669d504a9218f417fe3ac878688c2f2c5bc7ef19
Peter-Schorn <petervschorn@gmail.com>
2022-01-18 17:50:34 -0600

    Updated docc archive.

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -8,36 +8,9 @@ let app = Application(env)
 defer { app.shutdown() }
 app.http.server.configuration.hostname = "0.0.0.0"
 
-
-//#if DEBUG
-//
 guard let docCArchiveURL = docCArchiveURL else {
     fatalError("could not find SpotifyWebAPI.doccarchive in bundle")
 }
-//
-//#else  // RELEASE configuration on heroku
-
-//let path = "/app/.swift-bin/SpotifyAPIDocs_VaporDocC.resources"
-//
-//let contents = try FileManager.default.contentsOfDirectory(
-//    atPath: path
-//)
-//print("found \(contents.count) items in folder \(path)")
-//for file in contents {
-//    print(file)
-//}
-//print("---")
-//
-//guard let bundle = Bundle(path: path) else {
-//    fatalError("could not create bundle from path \(path)")
-//}
-//guard let docCArchiveURL = bundle.url(
-//    forResource: "SpotifyWebAPI", withExtension: "doccarchive"
-//) else {
-//    fatalError("could not find SpotifyWebAPI.doccarchive in bundle")
-//}
-
-//#endif
 
 app.logger.notice("docCArchiveURL: \(docCArchiveURL)")
 
